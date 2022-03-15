1c7aab8384bde4484e5a8940cad1279f9640a260
ivanmail <46161713+ivanmail@users.noreply.github.com>
2022-03-15 18:23:04 +0200

    2 uzduotis

---
+++ lesson2/uzduotys.php
@@ -0,0 +1,136 @@
+<?php
+
+// 1. Komandineje eiluteje panaudokite "php --help" komanda ir raskite kaip atidaryti PHP shell. Atidarykite ji.
+
+// Jei sekmingai atidarete shell'a - matysite "php >"
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// 2.
+
+// a) Panaudodami PHP shell, priskirkite 2 kintamiesiems boolean tipo reiksmes: vienam 'true', kitam 'false'.
+
+// b) Isspausdinkite pirmojo kintamojo reiksme pakeisdami boolean tipa i string ir su ta pacia komanda
+
+//    naujoje eiluteje isspausdinkite antrojo kintamojo reiksme pakeisdami boolean tipa i integer.
+
+// d) Uzdarykite PHP shell.
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// 3. Sia uzduoti atlikite panaudodami komandine eilute.
+
+// a) Sukurkite faila 'file.php'.
+
+// b) Pervardinkite si faila i 'index.php'
+
+// c) Sukurkite direktorija 'includes'
+
+// d) 'includes' direktorijoje sukurkite failus: header.php, content.php, footer.php
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// 4.
+
+// a) header.php faile isspausdinkite zodi 'header' ir nauja eilute
+
+// b) content.php faile isspausdinkite zodi 'content' ir nauja eilute
+
+// c) footer.php faile isspausdinkite zodu 'footer'
+
+// d) I index.php faila importuokite is eiles header.php, content.php, footer.php failus
+
+// e) Paleiskite index.php faila
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// 5. Paleiskite faila klaida1.php. Raskite kur yra klaida jame.
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// 6. Sukurkite faila age_validation.php
+
+// a) Apibrezkite konstanta MINIMUM_AGE ir priskirkite jai reiksme 18.
+
+// b) Priskirkite kintamajam "jonasAge" reiksme 13 ir kintamajam "povilasAge" reiksme 24.
+
+// c) Palyginkite ar siu kintamuju reiksmes yra lygios MINIMUM_AGE konstantos reiksmei.
+
+// d) Rezultatus isveskite var_dump() funkcijos pagalba.
+
+// e) Zemiau pakeiskite MINIMUM_AGE konstantos reiksme i 12. Ir vel atlikite c) ir d) veiksmus.
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// 7. Paleiskite faila klaida2.php. Raskite kur yra klaida jame.
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// 8.
+
+// a) Is savo gimimo metu atimkite savo amziu ir patikrinkite ar gautas skaicius dalinasi is 5.
+
+// b) Gauta boolean rezultata priskirkite  kintamajam "dalinasiIsPenkiu" ir isspausdinkite ji su var_dump().
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// 9. Sukurkite boolean tipo kintamuosius "baigeMokykla", "baigeUniversiteta" ir "baigeKursus". Kad dirbti programuotoju,
+
+// zmogus turi butinai baigti mokykla ir turi baigti bent viena is dvieju: universiteta arba kursus. Sukurkite boolean
+
+// kintamaji "galiDirbtiProgramuotoju", kurio reiksme butu apskaiciuota is auksciau isvardintu kintamuju.
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// 10.
+
+// a) Sukurkite vartotojo duomenu asociatyvu masyva $user. Priskirkite reiskmes tokiem masyvu raktams: vardas, pavarde,
+
+// gimimoMetai, lytis.
+
+// b) Isspausdinkite i viena eilute vartotojo duomenis tokiu formatu:
+
+// "Vardas: Ivan, Pavarde: Bondarev, Gimimo metai: 1990, lytis: vyras"
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// 11.
+
+// a) Sukurkite daugialypi vartotoju masyva $users. Pridekite i ji du skirtingus vartotoju masyvus is 10 uzduoties.
+
+// b) Isspausdinkite $users masyvo pirmo ir antro vartotoju duomenis atskiruose eilutese tokiu formatu:
+
+// "Vardas: Ivan, Pavarde: Bondarev, Gimimo metai: 1990, lytis: vyras"
+// "Vardas: Jonas, Pavarde: Jonaitis, Gimimo metai: 1985, lytis: vyras"
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// 12. Atidarykite hasErrors.php faila. Penktoje eiluteje irasykite koda, kuris isvestu zinute "Programa turi klaidu"
+
+// ir sustabdytu skripto veikima jei $hasErrors kintamojo reiksme lygi true. Kitu atveju turi but vykdomas apacioje
+
+// esantis kodas.
+
+
+// --------------------------------------------------------------------------------------------------------------------
